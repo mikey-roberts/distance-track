37179f743afa3bcb26855f39aa60dbb493fa42eb
mikey-roberts <mikey_roberts@outlook.com>
2021-10-13 12:52:29 +0100

    test: further tests added to check the response body for certain fields

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,12 +1,23 @@
 require 'rails_helper'
-
 describe UsersController do
+
+  render_views
+
   describe 'GET users' do
-    render_views
+    before(:each) do
+      get :index
+      @body = response.parsed_body
+    end
+
     it 'expect result to be successful' do
-      get "index"
       expect(response).to have_http_status(:success)
       expect(response).to have_http_status(200)
+    end
+
+    it 'response body to include' do
+      expect(response.body).to include('first_name')
+      expect(response.body).to include('last_name')
+      expect(response.body).to include('ip_address')
     end
   end
 end
